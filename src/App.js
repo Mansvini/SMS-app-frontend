48ca09fb6446c70abd0fd851884e7c05d60f5af6
Mansvini <coderspecial@gmail.com>
2022-05-18 12:56:13 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -11,34 +11,16 @@ import SendMessage from './Components/SendMessage';
 class App extends Component {
   render(){
     return (
-      // <Switch>
-      //   <Route exact path="/">
-      //     <Home />
-      //   </Route>
-      //   <Route path="/ContactList">
-      //     <ContactList />
-      //   </Route>
-      //   <Route path="/MessageHistoryList">
-      //     <MessageHistoryList />
-      //   </Route>
-      //   <Route path="/ContactInfo">
-      //     <ContactInfo />
-      //   </Route>
-      //   <Route path="/SendMessage">
-      //     <SendMessage />
-      //   </Route>
-      // </Switch>
-        <Routes>
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/" element={<Navigate replace to="/Home" />} />
-          <Route path="/ContactList" element={<ContactList/>} />
-          <Route path="/MessageHistoryList" element={<MessageHistoryList/>} />
-          <Route path="/ContactInfo" element={<ContactInfo />} />
-          <Route path="/SendMessage" element={<SendMessage />} />
-        </Routes>
+      <Routes>
+        <Route path="/Home" element={<Home/>} />
+        <Route path="/" element={<Navigate replace to="/Home" />} />
+        <Route path="/ContactList" element={<ContactList/>} />
+        <Route path="/MessageHistoryList" element={<MessageHistoryList/>} />
+        <Route path="/ContactInfo" element={<ContactInfo />} />
+        <Route path="/SendMessage" element={<SendMessage />} />
+      </Routes>
     );
   }
 }
-//try loaduser method to pass props to contact info hint: conditional on route, onclick change state, no link to
 
 export default App;
