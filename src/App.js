a0d3fb71ff6c2f5603694570fedc3c9a91de7b4b
Mansvini <coderspecial@gmail.com>
2022-05-18 13:06:36 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import 'tachyons';
 import Home from './Components/Home';
 import ContactList from './Components/ContactList';
 import MessageHistoryList from './Components/MessageHistoryList';
-import { BrowserRouter, Routes, Navigate, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Navigate, Route} from "react-router-dom";
 import ContactInfo from './Components/ContactInfo';
 import SendMessage from './Components/SendMessage';
 
 class App extends Component {
   render(){
     return (
-      <BrowserRouter>
-      <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/" element={<Navigate replace to="/Home" />} />
-        <Route path="/ContactList" element={<ContactList/>} />
-        <Route path="/MessageHistoryList" element={<MessageHistoryList/>} />
-        <Route path="/ContactInfo" element={<ContactInfo />} />
-        <Route path="/SendMessage" element={<SendMessage />} />
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/" element={<Navigate replace to="/Home" />} />
+          <Route path="/ContactList" element={<ContactList/>} />
+          <Route path="/MessageHistoryList" element={<MessageHistoryList/>} />
+          <Route path="/ContactInfo" element={<ContactInfo />} />
+          <Route path="/SendMessage" element={<SendMessage />} />
+        </Routes>
+      </Router>
     );
   }
 }
